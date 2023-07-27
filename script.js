4d0ff56b8475028c916f8e661c4b6d91d1a9e85b
[Keval Patel] <[khp90611@gmail.com]>
2023-07-27 14:37:34 -0400

    removed the previous attempts comments. Code is working and generates a password, I needed to define the password variable first before using it and now it is working fine, need to incorporate the special characters and select one from each minimum

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 
 let lowerCaseAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 let upperCaseAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let specialCharacters = ["!", "#", "$", "%", "&", ""];
 //Still need to add special character//
 
 //Default is lowercaseAlphabet
@@ -16,39 +17,40 @@ let builtArray = [];
 
 function generatePassword () {
 //Why does this only run and then stop without asking the prompt "How long do you want the password"//
-  let passwordLength = prompt("How long do you want the password");
-
-  if (confirm("Do you want your password to contain lowercase letters?")){
+  let passwordLength = prompt("How long do you want the password? Choose a number between 8 and 128");
+  let lowerCaseQuestion = confirm("Do you want your password to contain lowercase letters?") 
+  let upperCaseQuestion = confirm("Do you want your password to contain uppercase letters?")
+  let numberQuestion = confirm("Do you want your password to contain numbers?")
+  let password = "";
+  
+  if (lowerCaseQuestion){
       builtArray = [...builtArray,...lowerCaseAlphabet]
       }
 
-  if(confirm("Do you want your password to contain uppercase letters?")){
+  if(upperCaseQuestion){
       builtArray = [...builtArray, ...upperCaseAlphabet];
 
   }
   
-  if(confirm("Do you want your password to contain numbers?")){
+  if(numberQuestion){
       builtArray = [...builtArray, ...numbers];
 
   }
 
-  //Here I tried if - else statements instead originally
-  // if (upperCaseQuestion === true && numberQuestion === true){
-  //     let builtArray = [...builtArray, lowerCaseAlphabet, upperCaseAlphabet, numbers];
-  //     console.log(upperCaseQuestion)
-  // }
-  //   else if (upperCaseQuestion === false && numbers === true) {
-  //     let builtArray = [...builtArray, lowerCaseAlphabet, numbers];
-  //     console.log(upperCaseQuestion)
-  //   }
+  
 
-  //   else if (upperCaseQuestion ===true && numbers === false) {
-  //     let builtArray = [...builtArray, lowerCaseAlphabet, upperCaseAlphabet]
-  //     console.log(upperCaseQuestion)
-  //   }
-  //   else {
-  //     let builtArray = [...builtArray, lowerCaseAlphabet]
-  //     console.log(upperCaseQuestion)
+
+
+//Need an exception if user chooses no for all 
+if (lowerCaseQuestion=== false && upperCaseQuestion === false && numberQuestion === false){
+    alert("You must select at least one character type!! Click Regenerate Password!")
+    generatePassword();
+}
+
+if (passwordLength < 8 || passwordLength > 128){
+  alert("You must pick a number from 8 to 128 for the length of your password!! Click Regenerate Password Again")
+  generatePassword();
+}
     
 
 //Question: if I set the built Array's default as lowercase alphabet instead of blank would that make this code cleaner above//
@@ -59,7 +61,7 @@ function generatePassword () {
 
   for (let i = 0; i< passwordLength; i++){
     let generateLetterIndex = builtArray[Math.floor(Math.random()*builtArray.length)];
-    password += String(generateLetterIndex);
+    password += generateLetterIndex;
     // password += 
 
   // }
@@ -67,8 +69,10 @@ function generatePassword () {
 
 
 
+ 
 }
-return password
+ return password
+
 };
 
 function writePassword() {
